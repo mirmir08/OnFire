48fe3d87e991d2479a467c1fc8fbe5ef591a5399
mirmir08 <mmrm.miriam@gmail.com>
2025-11-09 14:13:57 -0600

    Firebasenalitycs

--- app/src/main/java/com/example/onfire/MainActivity.kt
+++ app/src/main/java/com/example/onfire/MainActivity.kt
@@ -5,8 +5,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.analytics.FirebaseAnalytics
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var analytics: FirebaseAnalytics
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +19,6 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        analytics = FirebaseAnalytics.getInstance(this)
     }
 }
